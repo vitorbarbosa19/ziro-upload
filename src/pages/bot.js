54a17af6e91fd159de1c92a039716c47d3f50ab9
VITOR BARBOSA <vitorbarbosa19@gmail.com>
2017-12-05 17:42:54 -0800

    starting integration with new bot API

--- src/pages/bot.js
+++ src/pages/bot.js
@@ -9,124 +9,26 @@ import UserInfo from '../components/UserInfo'
 import { containerStyle } from '../components/styles/styles'
 
 export default class Bot extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			igPremiumAccounts: null,
-			igNonPremiumAccounts: null,
-			newAccountName: '',
-			numberOfImagesToDownload: 5,
-			igImages: null,
-			downloadImages: true,
-			loading: false
-		}
-		this.newAccount = this.newAccount.bind(this)
-		this.saveNewAccount = this.saveNewAccount.bind(this)
-		this.removeAccount = this.removeAccount.bind(this)
-		this.updateNumberOfImagesToDownload = this.updateNumberOfImagesToDownload.bind(this)
-		this.fetchIgImages = this.fetchIgImages.bind(this)
-	}
-	componentDidMount() {
-		this.setState({
-			allIgAccounts: this.props.allBrandAccounts,
-			igPremiumAccounts: this.props.onlyPremiumBrandAccounts,
-			igNonPremiumAccounts: this.props.onlyNonPremiumBrandAccounts
+	async componentDidMount() {
+		const brandsData = await axios.get('https://zirobot.herokuapp.com/accounts')
+		const accountsToScrape = brandsData.data.values.splice(1, brandsData.data.values.length).map( (row) => {
+			return row[1]
 		})
-	}
-	newAccount(event) {
-		this.setState({
-			newAccountName: event.target.value
-		})
-	}
-	saveNewAccount() {
-		this.setState( (prevState) => {
-			allIgAccounts: prevState.allIgAccounts.splice(0, 0, this.state.newAccountName)
-		})
-	}
-	removeAccount(accountName) {
-		this.setState( (prevState) => {
-			allIgAccounts: prevState.allIgAccounts.splice(prevState.allIgAccounts.indexOf(accountName), 1)
-		})
-	}
-	updateNumberOfImagesToDownload(event) {
-		this.setState({
-			numberOfImagesToDownload: event.target.value
-		})
-	}
-	componentDidUpdate() {
-		if(this.state.downloadImages) {
-			const imagesUrls = document.getElementsByClassName('autoClick')
-			Array.prototype.map.call(imagesUrls, (imageUrl) => {
-				imageUrl.click() //force a click on the <a> tag so that the download triggers
-			})
-			this.setState({
-				downloadImages: false
-			})
-		}
-	}
-	fetchIgImages(accountName) {
-		this.setState({
-			loading: true
-		})
-		botApi.fetchImagesOneAccount(accountName, this.state.numberOfImagesToDownload)
-			.then( (response) => {
-				console.log(response.data)
-				this.setState({
-					igImages: response.data,
-					downloadImages: true,
-					loading: false
-				})
-			})
-			.catch( (error) => {
-				console.log(error)
-				alert(error)
-			})
+		//const test = ["limonemodas", "averarafashion", "talguistore", "luziafazzollioficial", "coinageoficial", "doceflorsp", "kessesoficial", "nuxxoficial", "amissimaoficial", "uniquechicoficial", "morinafashion", "donnaritzoficial", "karmanioficial"]
+		//const test = ["limonemodas"]
+		const test = ["limonemodas", "averarafashion", "talguistore", "luziafazzollioficial", "coinageoficial"]
+		const images = await Promise.all(test.map( async (igAccount) => {
+			return await axios.get(`https://zirobot.herokuapp.com/scrape?account=${igAccount}`)
+		}))
+		console.log(images)
 	}
 	render() {
 		return (
 			<div style={containerStyle}>		
-				{this.state.igImages ?
-					this.state.igImages.map( (image, index) => {
-						return (
-							<a 
-								className='autoClick'
-								style={{ visibility: 'hidden' }}
-								key={index}
-								href={image}
-								download={`${image}-${index}.jpg`}> {/*this property 'forces' the download of the image whose link is in the href property when the <a> tag is clicked */}
-							</a>
-						)
-					})
-				:
-					null
-				}
 				<UserInfo 
-					numberOfImagesToDownload={this.state.numberOfImagesToDownload}
+					numberOfImagesToDownload={4}
 					updateNumberOfImagesToDownload={this.updateNumberOfImagesToDownload}
 				/>
-				{/*<PremiumBrandList
-					igPremiumAccounts={this.state.igPremiumAccounts}
-					removeAccount={this.removeAccount}
-					fetchIgImages={this.fetchIgImages}
-					loading={this.state.loading}					
-				/>
-				<NonPremiumBrandList
-					igNonPremiumAccounts={this.state.igNonPremiumAccounts}
-					removeAccount={this.removeAccount}
-					fetchIgImages={this.fetchIgImages}
-					loading={this.state.loading}					
-				/>
-				<BrandList 
-					allIgAccounts={this.state.allIgAccounts} 
-					removeAccount={this.removeAccount}
-					fetchIgImages={this.fetchIgImages}
-					loading={this.state.loading}
-				/>
-				<InputNewBrand 
-					newAccountName={this.state.newAccountName} 
-					saveNewAccount={this.saveNewAccount} 
-					newAccount={this.newAccount}
-				/>*/}
 			</div>
 		)
 	}
